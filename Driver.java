7cb2df694b37c161d629bb616ece18482d938d28
sookiemonster <dsooknanan20@stuy.edu>
2020-12-17 15:08:37 -0500

    Added more tests for bubbleSort().

--- Driver.java
+++ Driver.java
@@ -11,5 +11,25 @@ public class Driver {
     Sorts.bubbleSort(b);
     System.out.println(Arrays.toString(b));
 
+    bubbleTest(10);
+    bubbleTest(0);
+    bubbleTest(1);
+    bubbleTest(2);
+    bubbleTest(20);
+  }
+
+  public static void randArray(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      int randNum =(int)(Math.random()*1000);
+      arr[i] = randNum;
+    }
+  }
+
+  public static void bubbleTest(int len) {
+    int[] arr = new int[len];
+    randArray(arr);
+    System.out.println(Arrays.toString(arr));
+    Sorts.bubbleSort(arr);
+    System.out.println(Arrays.toString(arr));
   }
 }
